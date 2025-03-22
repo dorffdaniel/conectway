402b445c60501a79f825024d904dd9a6a1bae55c
Dorffdaniel <danielcontabill@hotmail.com>
2025-03-22 12:12:43 +0000

    adicionado mais interacao no js na pagina busca e um button seja premium logo apos o login

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,13 @@ hamburguer.addEventListener('click', ()=>{
     hamburguer.classList.toggle('ativado')
     navList.classList.toggle('mostrar')
 })
+
+function conteudos(index){
+    let conteudoLi = document.querySelectorAll('.conteudosLi');
+
+    conteudoLi.forEach((event) =>{
+        event.classList.remove('aparecer');
+    })
+
+    conteudoLi[index].classList.add('aparecer');
+}
